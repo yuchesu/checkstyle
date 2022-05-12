2acbb2dc6b90868b3c397c875ee6e9ba9f40d40b
yuchesu <az75225@gmail.com>
2022-05-11 23:32:25 -0700

    try fix issue #1157

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlyTestForceLineBreakAfter.java
@@ -0,0 +1,8 @@
+package com.puppycrawl.tools.checkstyle.checks.blocks.rightcurly;
+
+public class InputRightCurlyTestForceLineBreakAfter
+{
+    Object o = new Object() {
+    }; int i = 0;  // violation ''}' at column 8 should be alone on a line'
+
+}
